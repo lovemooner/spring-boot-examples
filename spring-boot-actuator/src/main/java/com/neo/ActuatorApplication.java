09e093d06a91c05fe0fe318b2288a8539dcab0d0
lovemooner <lovemooner@yeah.com>
2019-04-02 17:55:38 +0800

    docker

--- spring-boot-actuator/src/main/java/com/neo/ActuatorApplication.java
+++ spring-boot-actuator/src/main/java/com/neo/ActuatorApplication.java
@@ -1,9 +1,14 @@
 package com.neo;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 public class ActuatorApplication {
 
 	public static void main(String[] args) {
