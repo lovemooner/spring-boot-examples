02532ead744d7dd66a7950d754a3a01ffb490b8c
dongnan <dongnan@kedacom.com>
2020-02-05 01:31:37 +0800

    tt

--- spring-boot-hello-world/src/main/java/love/moon/controller/TestController.java
+++ spring-boot-hello-world/src/main/java/love/moon/controller/TestController.java
@@ -4,14 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @auther dongnan
+ * @auther lovemooner
  * @date 2019/10/31 14:56
  * @describe
  */
 @RestController
 public class TestController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     public String hello() {
         return "hi";
     }
